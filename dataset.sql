9a9424b4196c071dab8c9f71fa65a6e930abef1c
ramparam1318 <65478206+ramparam1318@users.noreply.github.com>
2020-12-12 19:11:44 +0530

    Delete dataset.sql

--- dataset.sql
+++
@@ -1,8 +0,0 @@
-/*here i created the table with 3 variables server_id, timeofreading, temperature*/
-CREATE TABLE readings_dataset (
-    server_id INT,
-    timeofreading DATETIME,
-    temperature DECIMAL(10,2),
-    PRIMARY KEY (cage_id, dateofreading),
-    INDEX (dateofreading, cage_id) -- adding index, for good readability
-)
