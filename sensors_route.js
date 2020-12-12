85dc4dd9eef3f366aba332dbf821866c5dcf7ccf
ramparam1318 <65478206+ramparam1318@users.noreply.github.com>
2020-12-12 07:28:55 +0530

    Create sensors_route.js

---
+++ sensors_route.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const sensorSchema = new Schema({
+    _id: Schema.Types.sensor_id,
+    type: {
+      type: String,
+      required: true,
+      enum: ['Hydro', 'Thermo']
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+}, { timestamps: true } );
+
+module.exports = mongoose.model('Sensor', sensorSchema);
+
+//to get data from all sensors
+
+router.get('/', (req, res, next) => {
+  Sensor.find()
+    .select()
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        sensors: result.map(item => {
+          return item._doc
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+})
